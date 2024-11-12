9ff9a2b579a169248d735f68d092fc702321984f
Dzida <dominik.dzida@o2.pl>
2024-11-12 23:05:41 +0100

    Data saving and plot aniamtion

--- FileOperation.py
+++ FileOperation.py
@@ -1,21 +1,33 @@
 import sqlite3
-
-
+import pandas as pd
 
 class FileOperation:
 
     def __init__(self):
         self.path = 'TurtlesDataBase.db'
 
-    def if_exist(self)->bool:
+    def if_exist(self) -> None:
         conn = sqlite3.connect(self.path)
-        cursor = self.conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS Turtles (id INTEGER PRIMARY KEY, name TEXT, age INTEGER)''')
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Turtles (id INTEGER PRIMARY KEY, name TEXT)''')
+        conn.commit()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS Positions (id INTEGER PRIMARY KEY, name TEXT)''')
         conn.commit()
         conn.close()
-        return True
 
-    def insert(self):
-        cursor.execute('''INSERT INTO users (name, age) VALUES (?, ?)''', ('Alice', 30))
+    def insert_turtle(self)->bool:
+        conn = sqlite3.connect(self.path)
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO Turtles (id,name) VALUES (?, ?)''', ('Alice', 30))
+        conn.commit()
+        conn.close()
 
+    def insert_turtle_movment(self,data: pd.DataFrame)-> bool:
+        conn = sqlite3.connect(self.path)
+        cursor = conn.cursor()
+        cursor.execute('''INSERT INTO Positions (name, age) VALUES (?, ?)''', ('Alice', 30))
+        conn.commit()
+        conn.close()
 
+    def get_turtles(self)->pd.DataFrame:
+        pass
