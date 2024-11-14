11fac411e8570d45e23fcf1b724bc0a4122900f0
Dzida <dominik.dzida@o2.pl>
2024-11-15 00:39:12 +0100

    Turtle saving

--- DowloadTurtles.py
+++ DowloadTurtles.py
@@ -18,7 +18,7 @@ class DownloadTurtles:
         """
         try:
             response = requests.get("https://www.google.com", timeout=3)
-            self._connected = (response.status_code == 200)
+            self._connected = response.status_code == 200
         except requests.ConnectionError:
             self._connected = False
         except requests.Timeout:
@@ -27,7 +27,7 @@ class DownloadTurtles:
 
     def download_all_turtles_info(self) -> bool:
         """
-        
+
         :return:
         """
         if not self.check_connection():
@@ -40,40 +40,57 @@ class DownloadTurtles:
             data = response.json()
             turtles_data = []
             for turtle in data:
-                self._possible_turtles.update({turtle['id']: turtle['name']})
+                self._possible_turtles.update({turtle["id"]: turtle["name"]})
                 description = None
                 biography = None
                 project = None
-                for i in turtle['attributes']:
-                    if i['code'] == "Biography":
-                        biography = i['value']
-                    elif i['code'] == "Description":
-                        description = i['value']
-                    elif i['code'] == "Project":
-                        project = i['value']
+                for i in turtle["attributes"]:
+                    if i["code"] == "Biography":
+                        biography = i["value"]
+                    elif i["code"] == "Description":
+                        description = i["value"]
+                    elif i["code"] == "Project":
+                        project = i["value"]
 
-                if len(turtle['categories']) == 0:
+                if len(turtle["categories"]) == 0:
                     continue
-                turtles_data.append({
-                    'id': turtle['id'],
-                    'name': turtle['name'],
-                    'species': turtle['categories'][0]['name'],
-                    'last_move_datetime': turtle['motion']['last_move_datetime'],
-                    'distance_from_release': turtle['motion']['distance_from_release'],
-                    'avg_speed_from_release': turtle['motion']['avg_speed_from_release'],
-                    'time_from_last_move': turtle['motion']['time_from_last_move'],
-                    'time_from_release': turtle['motion']['time_from_release'],
-                    'time_tracked': turtle['motion']['time_tracked'],
-                    'description': description,
-                    'project': project,
-                    'biography': biography
-                })
-                self._full_data = pd.DataFrame(turtles_data,
-                                               columns=['id', 'name', 'species', 'last_move_datetime',
-                                                        'distance_from_release', 'avg_speed_from_release',
-                                                        'time_from_last_move', 'time_from_release', 'time_tracked',
-                                                        'description',
-                                                        'project', 'biography'])
+                turtles_data.append(
+                    {
+                        "id": turtle["id"],
+                        "name": turtle["name"],
+                        "species": turtle["categories"][0]["name"],
+                        "last_move_datetime": turtle["motion"]["last_move_datetime"],
+                        "distance_from_release": turtle["motion"][
+                            "distance_from_release"
+                        ],
+                        "avg_speed_from_release": turtle["motion"][
+                            "avg_speed_from_release"
+                        ],
+                        "time_from_last_move": turtle["motion"]["time_from_last_move"],
+                        "time_from_release": turtle["motion"]["time_from_release"],
+                        "time_tracked": turtle["motion"]["time_tracked"],
+                        "description": description,
+                        "project": project,
+                        "biography": biography,
+                    }
+                )
+                self._full_data = pd.DataFrame(
+                    turtles_data,
+                    columns=[
+                        "id",
+                        "name",
+                        "species",
+                        "last_move_datetime",
+                        "distance_from_release",
+                        "avg_speed_from_release",
+                        "time_from_last_move",
+                        "time_from_release",
+                        "time_tracked",
+                        "description",
+                        "project",
+                        "biography",
+                    ],
+                )
             return True
 
         except requests.exceptions.HTTPError as http_err:
@@ -108,23 +125,34 @@ class DownloadTurtles:
             response.raise_for_status()
             data = response.json()
             turtles_points = []
-            if 'results' in data:
-                for point in data['results']:
-                    point_data = point.get('data', {})
-                    turtles_points.append({
-                        'latitude': point_data.get('Lat', None),
-                        'longitude': point_data.get('Lng', None),
-                        'distance': point_data.get('Distance', None),
-                        'duration': point_data.get('Duration', None),
-                        'direction': point_data.get('Direction', None),
-                        'time': point_data.get('CollectedDateTime', None)
+            if "results" in data:
+                for point in data["results"]:
+                    point_data = point.get("data", {})
+                    turtles_points.append(
+                        {
+                            "latitude": point_data.get("Lat", None),
+                            "longitude": point_data.get("Lng", None),
+                            "distance": point_data.get("Distance", None),
+                            "duration": point_data.get("Duration", None),
+                            "direction": point_data.get("Direction", None),
+                            "time": point_data.get("CollectedDateTime", None),
+                        }
+                    )
 
-                    })
-
-                    self._last_downloaded_turtle = [pd.DataFrame(turtles_points,
-                                                                 columns=['latitude', 'longitude', 'distance',
-                                                                          'duration', 'direction', 'time']),
-                                                    self._full_data[self._full_data['id'] == 2]]
+                    self._last_downloaded_turtle = [
+                        pd.DataFrame(
+                            turtles_points,
+                            columns=[
+                                "latitude",
+                                "longitude",
+                                "distance",
+                                "duration",
+                                "direction",
+                                "time",
+                            ],
+                        ),
+                        self._full_data[self._full_data["id"] == 2],
+                    ]
 
             else:
                 raise ValueError
--- FileOperation.py
+++ FileOperation.py
@@ -1,33 +1,88 @@
 import sqlite3
 import pandas as pd
 
-class FileOperation:
 
+class FileOperation:
+    """
+
+    """
     def __init__(self):
-        self.path = 'TurtlesDataBase.db'
+        self.path = "TurtlesDataBase.db"
 
     def if_exist(self) -> None:
+        """
+
+        """
         conn = sqlite3.connect(self.path)
         cursor = conn.cursor()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS Turtles (id INTEGER PRIMARY KEY, name TEXT)''')
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS Turtles (
+                        id INTEGER PRIMARY KEY, 
+                        name TEXT,
+                        species TEXT,
+                        last_move_datetime TEXT,
+                        distance_from_release TEXT,
+                        avg_speed_from_release TEXT,
+                        time_from_release TEXT,
+                        time_from_last_move TEXT,
+                        time_tracked TEXT,
+                        description TEXT,
+                        project TEXT,
+                        biography TEXT
+                        )"""
+        )
         conn.commit()
-        cursor.execute('''CREATE TABLE IF NOT EXISTS Positions (id INTEGER PRIMARY KEY, name TEXT)''')
+        cursor.execute(
+            """CREATE TABLE IF NOT EXISTS Positions (
+            id INTEGER PRIMARY KEY,
+             latitude DOUBLE,
+             longitude DOUBLE,
+             distance DOUBLE,
+             duration DOUBLE,
+             direction TEXT,
+             time TEXT
+             )"""
+        )
         conn.commit()
         conn.close()
 
-    def insert_turtle(self)->bool:
+    def insert_turtle(self,turtle) -> bool:
+        """
+
+        :param turtle:
+        :return:
+        """
+        try:
+            conn = sqlite3.connect(self.path)
+            cursor = conn.cursor()
+            cursor.execute(
+                """INSERT INTO Turtles (id,name,species,last_move_datetime,distance_from_release,avg_speed_from_release,
+                time_from_release,time_from_last_move,time_tracked,description,project,biography) VALUES
+                 (?, ?,?, ?,?, ?,?, ?,?, ?,?, ?)""",
+                turtle,
+            )
+            conn.commit()
+            conn.close()
+            return True
+
+        except sqlite3.IntegrityError as e:
+            print(f"Integrity error occurred: {e}")
+        except sqlite3.OperationalError as e:
+            print(f"Operational error occurred: {e}")
+        except sqlite3.DatabaseError as e:
+            print(f"Database error occurred: {e}")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
+        return False
+
+    def insert_turtle_movement(self, data: pd.DataFrame) -> bool:
         conn = sqlite3.connect(self.path)
         cursor = conn.cursor()
-        cursor.execute('''INSERT INTO Turtles (id,name) VALUES (?, ?)''', ('Alice', 30))
+        cursor.execute(
+            """INSERT INTO Positions ( id,latitude ,longitude,distance ,duration,direction,time) VALUES (?, ?,?,?,?,?,?)""", data
+        )
         conn.commit()
         conn.close()
 
-    def insert_turtle_movment(self,data: pd.DataFrame)-> bool:
-        conn = sqlite3.connect(self.path)
-        cursor = conn.cursor()
-        cursor.execute('''INSERT INTO Positions (name, age) VALUES (?, ?)''', ('Alice', 30))
-        conn.commit()
-        conn.close()
-
-    def get_turtles(self)->pd.DataFrame:
+    def get_turtles(self) -> pd.DataFrame:
         pass
