ed3e5c8f1f1e4a1bb38c8c9309dbbcc0c41b2e40
Dzida <dominik.dzida@o2.pl>
2024-12-06 22:15:40 +0100

    flask basic test

---
+++ flask_test/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+fr
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ in "__main__":
+    app.run(debug=True)
